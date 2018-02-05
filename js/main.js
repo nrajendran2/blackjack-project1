6e7b1035220792d346b9fa847eadae46a643e6ef
mpotts0201 <mpotts0201@gmail.com>
2018-02-05 16:54:57 -0500

    added card array and start function

---
+++ js/main.js
@@ -0,0 +1,345 @@
+const cards = [
+    //HEARTS
+    {
+        suit: "hearts",
+        name: "ace",
+        image: '',
+        value: 11
+    },
+    {
+        suit: "hearts",
+        name: "two",
+        image: '',
+        value: 2
+    },
+    {
+        suit: "hearts",
+        name: "three",
+        image: '',
+        value: 3
+    },
+    {
+        suit: "hearts",
+        name: "four",
+        image: '',
+        value: 4
+    },
+    {
+        suit: "hearts",
+        name: "five",
+        image: '',
+        value: 5
+    },
+    {
+        suit: "hearts",
+        name: "six",
+        image: '',
+        value: 6
+    },
+    {
+        suit: "hearts",
+        name: "seven",
+        image: '',
+        value: 7
+    },
+    {
+        suit: "hearts",
+        name: "eight",
+        image: '',
+        value: 8
+    },
+    {
+        suit: "hearts",
+        name: "nine",
+        image: '',
+        value: 9
+    },
+    {
+        suit: "hearts",
+        name: "ten",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "hearts",
+        name: "jack",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "hearts",
+        name: "queen",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "hearts",
+        name: "king",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "diamonds",
+        name: "ace",
+        image: '',
+        value: 11
+    },
+    {
+        suit: "diamonds",
+        name: "two",
+        image: '',
+        value: 2
+    },
+    {
+        suit: "diamonds",
+        name: "three",
+        image: '',
+        value: 3
+    },
+    {
+        suit: "diamonds",
+        name: "four",
+        image: '',
+        value: 4
+    },
+    {
+        suit: "diamonds",
+        name: "five",
+        image: '',
+        value: 5
+    },
+    {
+        suit: "diamonds",
+        name: "six",
+        image: '',
+        value: 6
+    },
+    {
+        suit: "diamonds",
+        name: "seven",
+        image: '',
+        value: 7
+    },
+    {
+        suit: "diamonds",
+        name: "eight",
+        image: '',
+        value: 8
+    },
+    {
+        suit: "diamonds",
+        name: "nine",
+        image: '',
+        value: 9
+    },
+    {
+        suit: "diamonds",
+        name: "ten",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "diamonds",
+        name: "jack",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "diamonds",
+        name: "queen",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "diamonds",
+        name: "king",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "clubs",
+        name: "ace",
+        image: '',
+        value: 11
+    },
+    {
+        suit: "clubs",
+        name: "two",
+        image: '',
+        value: 2
+    },
+    {
+        suit: "clubs",
+        name: "three",
+        image: '',
+        value: 3
+    },
+    {
+        suit: "clubs",
+        name: "four",
+        image: '',
+        value: 4
+    },
+    {
+        suit: "clubs",
+        name: "five",
+        image: '',
+        value: 5
+    },
+    {
+        suit: "clubs",
+        name: "six",
+        image: '',
+        value: 6
+    },
+    {
+        suit: "clubs",
+        name: "seven",
+        image: '',
+        value: 7
+    },
+    {
+        suit: "clubs",
+        name: "eight",
+        image: '',
+        value: 8
+    },
+    {
+        suit: "clubs",
+        name: "nine",
+        image: '',
+        value: 9
+    },
+    {
+        suit: "clubs",
+        name: "ten",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "clubs",
+        name: "jack",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "clubs",
+        name: "queen",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "clubs",
+        name: "king",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "spades",
+        name: "ace",
+        image: '',
+        value: 11
+    },
+    {
+        suit: "spades",
+        name: "two",
+        image: '',
+        value: 2
+    },
+    {
+        suit: "spades",
+        name: "three",
+        image: '',
+        value: 3
+    },
+    {
+        suit: "spades",
+        name: "four",
+        image: '',
+        value: 4
+    },
+    {
+        suit: "spades",
+        name: "five",
+        image: '',
+        value: 5
+    },
+    {
+        suit: "spades",
+        name: "six",
+        image: '',
+        value: 6
+    },
+    {
+        suit: "spades",
+        name: "seven",
+        image: '',
+        value: 7
+    },
+    {
+        suit: "spades",
+        name: "eight",
+        image: '',
+        value: 8
+    },
+    {
+        suit: "spades",
+        name: "nine",
+        image: '',
+        value: 9
+    },
+    {
+        suit: "spades",
+        name: "ten",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "spades",
+        name: "jack",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "spades",
+        name: "queen",
+        image: '',
+        value: 10
+    },
+    {
+        suit: "spades",
+        name: "king",
+        image: '',
+        value: 10
+    }
+];
+
+const gameBoard = [];
+
+
+
+function deal(){
+    let num = Math.floor(Math.random()*cards.length)
+    gameBoard.forEach(($each)=>{
+        if(cards[num] !== gameBoard[$each]){
+            console.log(gameBoard.push(cards[num]))
+            console.log(gameBoard)
+        }
+    })
+} 
+console.log(deal())
+
+function start(){
+    $('#start').on('click', ()=>{
+        setBoard();
+    })
+}
+
+function setBoard(){
+    // $('#start').hide()
+    // console.log(cards)
+
+
+}
+
+start();
